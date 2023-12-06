56d5db1d5d74342a71f6a774f8f013023551cd04
Olga Smirnova <almazkacode@gmail.com>
2023-12-06 16:05:55 +0600

    feat: add burger menu

--- js/modules/burger.js
+++ js/modules/burger.js
@@ -1,10 +1,12 @@
 const burger = document.querySelector('.burger');
 const menuBurger = document.querySelector('.header__nav');
-const menuLinks = document.querySelectorAll('.nav__link');
+const menuLinks = document.querySelectorAll('#link');
+const body = document.querySelector('.body');
 
 const toggleMenu = function () {
   burger.classList.toggle('burger--active');
   menuBurger.classList.toggle('header__nav--active');
+  body.classList.toggle('stop-scroll');
 }
 
 burger.addEventListener('click', () => {
@@ -15,5 +17,6 @@ menuLinks.forEach(function(el) {
   el.addEventListener('click', function() {
     burger.classList.remove('burger--active');
     menuBurger.classList.remove('header__nav--active');
+    body.classList.remove('stop-scroll');
   });
 });
